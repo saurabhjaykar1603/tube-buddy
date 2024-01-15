9ff0ff344afae9abb9a5a11e38be63804289e65e
saurabhjaykar1603 <jaykarsaurabh97@gmail.com>
2024-01-15 16:32:13 +0530

    express server connected

--- server/index.js
+++ server/index.js
@@ -1,5 +1,18 @@
-import connectDB from "./db/index.js"
+import connectDB from "./db/index.js";
 import dotenv from "dotenv";
-dotenv.config(
-)
+import { app } from "./src/app.js";
+dotenv.config();
+const PORT = process.env.PORT || 8000;
 connectDB()
+  .then(() => {
+    app.on("error", (error) => {
+      console.log("ERRR:", error);
+      throw error;
+    });
+    app.listen(PORT, () => {
+      console.log(`Server is Listening on PORT ${PORT}`);
+    });
+  })
+  .catch((err) => {
+    console.log(`mongodb  faild error`, err);
+  });
